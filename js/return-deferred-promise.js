684077b1b8d69113dc92995c90a06bccb4e5e335
Adam Silverstein <adam@10up.com>
2014-02-20 19:17:12 -0800

    Fix Jshint warnings; switch to then() for all even handling

--- js/return-deferred-promise.js
+++ js/return-deferred-promise.js
@@ -15,8 +15,8 @@ $( document ).ready( function(){
 			deferred = $.Deferred(), percentComplete,
 			key = _.random( 1111111111111, 9999999999999 );
 
-		$( '.inside' ).append( '<div class="deferred-box ' + key + '" style="left:' + randomPosition() +
-								'px; top: ' + randomPosition() + 'px;"></div>' );
+		$( '.inside' ).append( '<a href="#" class="deferred-box ' + key + '" style="left:' + randomPosition() +
+								'px; top: ' + randomPosition() + 'px;"></a>' );
 		thisBox = $( '.' + key );
 		thisBox.fadeOut().fadeIn( _.random( 500, 3000 ) );
 
@@ -27,6 +27,7 @@ $( document ).ready( function(){
 			if ( 'resolved' !== deferred.state() ){
 				if ( 'rgb(0, 0, 0)' === newColor || 'black' === newColor ) {
 					$this.css( 'background-color', newColor );
+					deferred.notify();
 					deferred.resolve();
 				} else {
 					$this.css( 'background-color', newColor );
@@ -37,25 +38,22 @@ $( document ).ready( function(){
 			event.stopPropagation();
 			if ( ! moving ){
 				moving = true;
+				horizontalPlusorminus = ( 0 === _.random( 0, 1 ) ? '+' : '-' );
+				$this = $( this );
+				position = $this.offset();
 
-				if ( 'resolved' !== deferred.state() ){
-					horizontalPlusorminus = ( 0 == _.random( 0, 1 ) ? '+' : '-' );
-					$this = $( this );
-					position = $this.offset();
+				horizontalPlusorminus = 50 >= position.left ? '+' : horizontalPlusorminus;
+				horizontalPlusorminus = 449 < position.left ? '-' : horizontalPlusorminus;
+				verticalPlusorminus = ( 0 === _.random( 0, 1 ) ? '+' : '-' );
+				verticalPlusorminus = 50 >= position.top ? '+' : verticalPlusorminus;
+				verticalPlusorminus = 440 < position.top ? '-' : verticalPlusorminus;
 
-					horizontalPlusorminus = 50 >= position.left ? '+' : horizontalPlusorminus;
-					horizontalPlusorminus = 449 < position.left ? '-' : horizontalPlusorminus;
-					verticalPlusorminus = ( 0 == _.random( 0, 1 ) ? '+' : '-' );
-					verticalPlusorminus = 50 >= position.top ? '+' : verticalPlusorminus;
-					verticalPlusorminus = 440 < position.top ? '-' : verticalPlusorminus;
-
-					$this.delay( 500 ).animate({
-						'left': horizontalPlusorminus + '=50px',
-						'top':  verticalPlusorminus   + '=50px'
-						 }, 1000, function() {
-						 	moving = false;
-						 } );
-				}
+				$this.delay( 500 ).animate({
+					'left': horizontalPlusorminus + '=50px',
+					'top':  verticalPlusorminus   + '=50px'
+					}, 1000, function() {
+						moving = false;
+					} );
 			}
 		});
 		return deferred.promise();
@@ -71,7 +69,8 @@ $( document ).ready( function(){
 
 		return ( completed / total * 100 ) + "%";
 
-	}
+	};
+
 
 	// Main function add some boxes
 	console.log ( 'adding boxes...' );
@@ -82,14 +81,22 @@ $( document ).ready( function(){
 		} else {
 			allThePromises = $.when( deferredBox, allThePromises );
 		}
-		deferredBox.done( function(){
-			$( '.progress-complete' ).css( 'width', percentComplete );
-		});
 	}
 
-	allThePromises.done( function(){
-		console.log( 'finished!' );
-		$( '.progress-complete' ).css( 'background-color', '#f00' );
-	})
+	allThePromises.then(
+		// done
+		function(){
+			console.log( 'finished!' );
+			$( '.progress-complete' ).css( 'background-color', '#f00' );
+		},
+		// fail
+		function(){
+
+		},
+		// progress
+		function(){
+			$( '.progress-complete' ).css( 'width', percentComplete );
+		}
+	);
 
 });
